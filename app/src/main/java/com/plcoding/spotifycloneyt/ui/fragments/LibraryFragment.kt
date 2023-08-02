a7f0f065429bb4a5d40128eaccc6a8ad505574f3
SOMESH <someshzade99@gmail.com>
2023-08-02 14:08:42 +0530

    Master-Commit

---
+++ app/src/main/java/com/plcoding/spotifycloneyt/ui/fragments/LibraryFragment.kt
@@ -0,0 +1,105 @@
+package com.plcoding.spotifycloneyt.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.plcoding.spotifycloneyt.R
+import com.plcoding.spotifycloneyt.adapters.SongAdapter
+import com.plcoding.spotifycloneyt.other.Status
+import com.plcoding.spotifycloneyt.ui.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_home.*
+import javax.inject.Inject
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LibraryFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+@AndroidEntryPoint
+class LibraryFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    lateinit var mainViewModel: MainViewModel
+
+    @Inject
+    lateinit var songAdapter: SongAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_library, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        var button=view.findViewById<ImageView>(R.id.plus_button)
+        button.setOnClickListener{
+            navHostFragment.findNavController().navigate(
+                R.id.globalActionToSelectSongFragmentList
+            )
+        }
+        setupRecyclerView()
+        subscribeToObservers()
+        songAdapter.setItemClickListener {
+            mainViewModel.playOrToggleSong(it)
+        }
+    }
+
+    private fun setupRecyclerView() = rvAllSongs.apply {
+        adapter = songAdapter
+        layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun subscribeToObservers() {
+        mainViewModel.songs.observe(viewLifecycleOwner) { result ->
+            songAdapter.songs = result
+        }
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment LibraryFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            LibraryFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
